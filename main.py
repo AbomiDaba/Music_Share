2f88315ea356292ae2fca2d210f53bec1ebf293c
Abomi Daba <abomi.daba@gmail.com>
2023-01-09 15:36:52 -0800

    updated app host

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from flask_app import app
 from flask_app.controllers import users, songs
 
 if __name__ == ('__main__'):
-    app.run(debug=False)
+    app.run(debug=False, host = '0.0.0.0', port= 5000)
